d462ca5a112488fa47e2378195aeef0398bee8c3
Rakesh K <codersgyan@gmail.com>
2024-04-10 12:11:28 +0300

    add shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
     return (
         <main>
             <h1>Welcome to pizza shop</h1>
+            <Button variant="outline">Button</Button>
         </main>
     );
 }
