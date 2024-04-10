a7862ac4fa96404d10423c2dc6bcd8e6ddee911e
Rakesh K <codersgyan@gmail.com>
2024-04-10 13:31:52 +0300

    add hero section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,28 @@
+import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 
 export default function Home() {
     return (
         <>
-            <h1>Welcome to pizza shop</h1>
-            <Button>Button</Button>
+            <section className="bg-white">
+                <div className="container flex items-center justify-between py-24">
+                    <div>
+                        <h1 className="text-7xl font-black font-sans leading-2">
+                            Super Delicious Pizza in <br />
+                            <span className="text-primary">Only 45 Minutes!</span>
+                        </h1>
+                        <p className="text-2xl mt-8 max-w-lg leading-snug">
+                            Enjoy a Free Meal if Your Order Takes More Than 45 Minutes!
+                        </p>
+                        <Button className="mt-8 text-lg rounded-full py-7 px-6 font-bold">
+                            Get your pizza now
+                        </Button>
+                    </div>
+                    <div>
+                        <Image alt="pizza-main" src={'/pizza-main.png'} width={400} height={400} />
+                    </div>
+                </div>
+            </section>
         </>
     );
 }
