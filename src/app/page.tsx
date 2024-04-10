e9877fbdb8e0b0764fe212a951dcf1fd7a67e12d
Rakesh K <codersgyan@gmail.com>
2024-04-10 12:16:55 +0300

    add shadcn ui theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <main>
             <h1>Welcome to pizza shop</h1>
-            <Button variant="outline">Button</Button>
+            <Button>Button</Button>
         </main>
     );
 }
